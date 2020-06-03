3caff9ec901a73de4b0381283f92bfa3b4f3c3d4
Douglas Delapria <delapria@gmail.com>
2020-06-02 22:00:36 -0300

    Desafio 05: Primeiro projeto Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (acum, transaction) =>
+        acum + (transaction.type === 'income' ? transaction.value : 0),
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (acum, transaction) =>
+        acum + (transaction.type === 'outcome' ? transaction.value : 0),
+      0,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
